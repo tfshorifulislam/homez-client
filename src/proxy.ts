6ae8faca3dfb27408a766d6a57323a1394fa9374
tfshorifulislam <tfshorifulislam@gmail.com>
2026-07-13 15:45:05 +0600

    proxy file issue fixed

--- src/proxy.ts
+++ src/proxy.ts
@@ -14,5 +14,5 @@ export async function proxy(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/dashboard","/profile","/buyer/wishlist","/seller/addproperty","/seller/my-properties","/admin/users","/admin/properties"],
+  matcher: ["/dashboard", "/profile", "/buyer/wishlist", "/seller/addproperty", "/seller/my-properties", "/admin/users", "/admin/properties"]
 };
